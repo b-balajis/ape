386ce6c28b02a39c44a67e19cbd8957e4acc460a
Balaji <balajibheemavarapu@gmail.com>
2023-03-03 12:02:33 +0530

    comments

--- src/api/ApiCall.js
+++ src/api/ApiCall.js
@@ -1,34 +1,34 @@
-import { useEffect } from 'react'
-import useHttp from "../hooks/use-http";
-import { useDispatch } from 'react-redux';
-import * as appactions from "../store/modules/app/slices/app.slice"
-// import { renderSubjects, renderPresentSem } from "../../api";
+// import { useEffect } from 'react'
+// import useHttp from "../hooks/use-http";
+// import { useDispatch } from 'react-redux';
+// import * as appactions from "../store/modules/app/slices/app.slice"
+// // import { renderSubjects, renderPresentSem } from "../../api";
 
-const Apicalling = (props) => {
-    const dispatch = useDispatch();
-    console.log(props);
-    const api = props.api;
-    const payload= props.payload
-    const { sendRequest, status, data: loadedData, error } = useHttp(api, true);
+// const Apicalling = (props) => {
+//     const dispatch = useDispatch();
+//     console.log(props);
+//     const api = props.api;
+//     const payload= props.payload
+//     const { sendRequest, status, data: loadedData, error } = useHttp(api, true);
 
 
-    useEffect(() =>(
-        sendRequest(payload)
-    ), [status])
+//     useEffect(() =>(
+//         sendRequest(payload)
+//     ), [status])
 
-    console.log(status, loadedData, error);
+//     console.log(status, loadedData, error);
 
-    const Output = [status, loadedData, error];
-     let cout = 0
-    if (status === "completed"){
-        console.log("yes");
-        cout = cout + 1;
-        console.log(cout);
-        dispatch(appactions.apiresponse(Output))
-    }
+//     const Output = [status, loadedData, error];
+//      let cout = 0
+//     if (status === "completed"){
+//         console.log("yes");
+//         cout = cout + 1;
+//         console.log(cout);
+//         dispatch(appactions.apiresponse(Output))
+//     }
 
-    console.log(Output);
-    // return status;
-}
+//     console.log(Output);
+//     // return status;
+// }
 
-export default Apicalling;
+// export default Apicalling;
